bd52d9dd3ebb49681c92f16a97486a8995ac7497
m3tro1d <qazwf734@gmail.com>
2020-07-18 10:18:05 +0300

    Add config parameters parsing

--- pybackup.py
+++ pybackup.py
@@ -8,6 +8,21 @@ config = configparser.ConfigParser()
 config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
 	"pybackup.ini")
 config.read(config_path)
-print(config.sections())
+
+
+# Get the config values, but skip the DEFAULT
+archive_names = []
+dirs_names = []
+for key in list(config.keys())[1:]:
+	if key.startswith("archive"):
+		# Get the archive name
+		archive_names.append(config[key]["name"])
+	elif key.startswith("directories"):
+		# Get all the directories recutsively
+		current_dirs = []
+		for dir_key in config[key]:
+			current_dirs.append(config[key][dir_key])
+		dirs_names.append(current_dirs)
+
 
 # Zip the directories to the specified archives
