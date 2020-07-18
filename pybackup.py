8f7622769eb59589a3bcfa3dc80c16fe8bd139a1
m3tro1d <qazwf734@gmail.com>
2020-07-18 10:09:02 +0300

    Change tab

--- pybackup.py
+++ pybackup.py
@@ -6,7 +6,7 @@ import zipfile
 # Parse the config file
 config = configparser.ConfigParser()
 config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-    "pybackup.ini")
+	"pybackup.ini")
 config.read(config_path)
 print(config.sections())
 
