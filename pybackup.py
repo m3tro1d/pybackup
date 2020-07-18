b97e46dcb78413d931c1c50ec7768a2d177e6fdf
m3tro1d <qazwf734@gmail.com>
2020-07-18 10:02:21 +0300

    Change ini file path

--- pybackup.py
+++ pybackup.py
@@ -1,10 +1,13 @@
 import configparser
+import os
 import zipfile
 
 
 # Parse the config file
 config = configparser.ConfigParser()
-config.read("pybackup.ini")
+config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+    "pybackup.ini")
+config.read(config_path)
 print(config.sections())
 
 # Zip the directories to the specified archives
