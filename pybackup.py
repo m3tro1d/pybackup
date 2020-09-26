5b15ce00f8d56090977dad0b7a18be69be00393e
Leoni Franco Paz <71673718+leonifrancopaz@users.noreply.github.com>
2020-09-26 15:18:18 -0300

    Update pybackup.py
    Added some comments and edited some things.

--- pybackup.py
+++ pybackup.py
@@ -6,8 +6,15 @@ import sys
 import zipfile
 
 
+# Constants for avoiding "magic" values.
+DEFAULT_COMPRESSION_LEVEL = 0
+DEFAULT_COMPRESSION_METHOD = "ZIP_STORED"
+
+
 def zip_dir_rec(dirname, archive, verbose):
     """Zips a directory with all files recursively"""
+    # This is very confusing, also what if archive is not an instance of zipfile?
+    # Maybe try using isinstance(archive, zipfile.ZipFile)?
     for entry in os.scandir(dirname):
         # Scan directories recursively
         if os.path.isdir(entry):
@@ -19,37 +26,32 @@ def zip_dir_rec(dirname, archive, verbose):
 
 
 def get_compression_settings(config):
-    """Returns a tuple of (compression_method, compression_level)"""
-    """And handles possible exceptions"""
-    methods = [
-        zipfile.ZIP_STORED,
-        zipfile.ZIP_DEFLATED,
-        zipfile.ZIP_BZIP2,
-        zipfile.ZIP_LZMA
-    ]
+    """Return a tuple of (compression_level, compression_method)."""
+    # Another issue: what if config is not an instance of config.ConfigParser?
+    # I guess this could be a class method?
+    # Or maybe run this code only as a script to avoid issues.
+    
+    # Switch-case-like dictionary.
+    method_constant = {"ZIP_STORED": zipfile.ZIP_STORED,
+                       "ZIP_DEFLATED": zipfile.ZIP_DEFLATED,
+                       "ZIP_BZIP2": zipfile.ZIP_BZIP2,
+                       "ZIP_LZMA": zipfile.ZIP_LZMA}
+
+    # Get compression level.
     try:
-        # Get the values & handle exceptions
-        cmp_method = methods[int(config["general"]["compression_method"])]
-        cmp_lvl = int(config["general"]["compression_level"])
+        compresion_level = config.getint("compression", "level")
+    except ValueError:
+        print("Invalid compression level, using default value.")
+        compression_level = DEFAULT_COMPRESSION_LEVEL
 
-        # Check the level
-        if not 0 <= cmp_lvl < len(methods):
-            raise IndexError("Invalid compression level {}".format(cmp_lvl))
+    # Get compression method.
+    try:
+        compression_method = method_constant[config["compression"]["method"]]
+    except KeyError:
+        print("Invalid compression method, using default value.")
+        compression_method = DEFAULT_COMPRESSION_METHOD
 
-    except ValueError as er:
-        # Handle invalid types (e.g. not numbers)
-        print("Invalid compression settings format: {}".format(er))
-        sys.exit(1)
-
-    except IndexError as er:
-        # Handle invalid values
-        print("Error!")
-        print("Invalid compression values: {}".format(er))
-        print("Using the default values (method: 1 ZIP_DEFLATED; lvl: 5).\n")
-        cmp_method = methods[1]
-        cmp_lvl = 5
-
-    return (cmp_method, cmp_lvl)
+    return compression_level, compression_method
 
 
 def append_date(filename):
@@ -61,6 +63,7 @@ def append_date(filename):
     return "{}-{}.{}".format(basename, str_date, ext)
 
 
+# This could run on a function or class, or under the if clause at the end.
 # Parse the input arguments
 parser = argparse.ArgumentParser(
     description="""A simple backup routine in python.""")
@@ -78,11 +81,13 @@ verbose = args.verbose
 
 # Parse the config file
 config = configparser.ConfigParser()
-config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           "pybackup.ini")
-config.read(config_path)
+# This is not necessary.
+#config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+#                          "pybackup.ini")
+config.read("pybackup.ini")
 
 
+# Why is this here?
 # Parse the settings from config file
 # General overall settings
 compression_method, compression_level = get_compression_settings(config)
@@ -91,15 +96,18 @@ compression_method, compression_level = get_compression_settings(config)
 archive_names = []
 dirs_names = []
 # Skip the DEFAULT section
-for key in list(config.keys())[1:]:
-    if key.startswith("archive"):
+# Making this a little more readable?
+# Also, why list()? It's not like you're modifying the view-like object from .keys().
+config_sections = list(config.keys())[1:]
+for section in config_sections:
+    if section.startswith("archive"):
         # Get the archive name
         # Append with date if needed
         if date_flag:
-            archive_names.append(append_date(config[key]["name"]))
+            archive_names.append(append_date(config[section]["name"]))
         else:
-            archive_names.append(config[key]["name"])
-    elif key.startswith("directories"):
+            archive_names.append(config[section]["name"])
+    elif section.startswith("directories"):
         # Get all the directories
         current_dirs = []
         for directory in config[key].values():
@@ -107,6 +115,8 @@ for key in list(config.keys())[1:]:
         dirs_names.append(current_dirs)
 
 
+# Either im lazy or this is borderline unreadable.
+# Shouldn't this be a function?
 # Add the directories to the specified archives
 for archive_name, target_dirs in zip(archive_names, dirs_names):
     # Initialize a ZipFile
@@ -132,3 +142,10 @@ for archive_name, target_dirs in zip(archive_names, dirs_names):
 
     # Close the file
     archive_file.close()
+
+
+# If the code is run as a script then it should run under this if clause.
+if __name__ == "__main__":
+    # Read configuration.
+    # Do things with zipfile.
+    pass
