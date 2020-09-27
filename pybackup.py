6ecb6dbbe577acc6c993d4131aafc7d792ef31b1
m3tro1d <qazwf734@gmail.com>
2020-09-27 12:58:00 +0300

    Put everything in separate functions

--- pybackup.py
+++ pybackup.py
@@ -11,6 +11,30 @@ DEFAULT_COMPRESSION_LEVEL = 0
 DEFAULT_COMPRESSION_METHOD = "ZIP_STORED"
 
 
+def parse_arguments():
+    """Process & return an argparse.ArgumentParser object"""
+    parser = argparse.ArgumentParser(
+        description="""A simple backup routine in python.""")
+
+    parser.add_argument("--date", "-d", action="store_true",
+                        help="append archive names with current date")
+
+    parser.add_argument("--verbose", "-v", action="store_true",
+                        help="be verbose, e.g. print all archived files")
+
+    args = parser.parse_args()
+    return args
+
+
+def config_init():
+    """Return a configparser.ConfigParser object with current options"""
+    config = configparser.ConfigParser()
+    config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                               "pybackup.ini")
+    config.read(config_path)
+    return config
+
+
 def zip_dir_rec(dirname, archive, verbose):
     """Zips a directory with all files recursively"""
     # Scan directories recursively
@@ -50,6 +74,31 @@ def get_compression_settings(config):
     return compression_level, compression_method
 
 
+def get_archives_settings(config):
+    """Return two lists: archive_names, dirs_names"""
+    archive_names = []
+    dirs_names = []
+
+    # Skip the DEFAULT section (using list to make it subscriptable)
+    config_sections = list(config.keys())[1:]
+    for section in config_sections:
+        if section.startswith("archive"):
+            # Get the archive name
+            # Append with date if needed
+            if date_flag:
+                archive_names.append(append_date(config[section]["name"]))
+            else:
+                archive_names.append(config[section]["name"])
+        elif section.startswith("directories"):
+            # Get all the directories
+            current_dirs = []
+            for directory in config[section].values():
+                current_dirs.append(directory)
+            dirs_names.append(current_dirs)
+
+    return archive_names, dirs_names
+
+
 def append_date(filename):
     """Appends current date (dd-mm-yy) to the filename"""
     basename = ".".join(filename.split(".")[:-1])
@@ -59,27 +108,43 @@ def append_date(filename):
     return "{}-{}.{}".format(basename, str_date, ext)
 
 
-# TODO put this to a separate function
+def run_backup(archive_names, dirs_names, cmp_method, cmp_level):
+    """Actual archiving happens here"""
+    # Add the directories to the specified archives
+    for archive_name, target_dirs in zip(archive_names, dirs_names):
+        # Initialize a ZipFile
+        archive_file = zipfile.ZipFile(archive_name, 'w',
+            cmp_method, compresslevel=cmp_level)
+        # Log the archive file
+        print("Archiving to {}:".format(os.path.basename(archive_name)))
+
+        # Loop through the directories
+        for target_dir in target_dirs:
+            # Check if the directory exists
+            if os.path.isdir(target_dir):
+                # Get the needed dirnames
+                parent_dir = os.path.dirname(target_dir)
+                archived_dir = os.path.basename(target_dir)
+                os.chdir(parent_dir)
+                # Log the action
+                print("  + {}".format(archived_dir))
+                # Zip the directories relatively to their parent directory
+                zip_dir_rec(archived_dir, archive_file, verbose)
+            else:
+                print("'{}' is not a directory.".format(target_dir))
+
+        # Close the file
+        archive_file.close()
+
+
 # Parse the input arguments
-parser = argparse.ArgumentParser(
-    description="""A simple backup routine in python.""")
-
-parser.add_argument("--date", "-d", action="store_true",
-                    help="append archive names with current date")
-
-parser.add_argument("--verbose", "-v", action="store_true",
-                    help="be verbose, e.g. print all archived files")
-
-args = parser.parse_args()
+args = parse_arguments()
 date_flag = args.date
 verbose = args.verbose
 
 
 # Parse the config file
-config = configparser.ConfigParser()
-config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           "pybackup.ini")
-config.read(config_path)
+config = config_init()
 
 
 # Parse the settings from config file
@@ -87,52 +152,11 @@ config.read(config_path)
 compression_method, compression_level = get_compression_settings(config)
 
 # Archiving settings
-archive_names = []
-dirs_names = []
-# Skip the DEFAULT section (using list to make it subscriptable)
-config_sections = list(config.keys())[1:]
-for section in config_sections:
-    if section.startswith("archive"):
-        # Get the archive name
-        # Append with date if needed
-        if date_flag:
-            archive_names.append(append_date(config[section]["name"]))
-        else:
-            archive_names.append(config[section]["name"])
-    elif section.startswith("directories"):
-        # Get all the directories
-        current_dirs = []
-        for directory in config[section].values():
-            current_dirs.append(directory)
-        dirs_names.append(current_dirs)
+archive_names, dirs_names = get_archives_settings(config)
 
 
-# TODO put this to a separate function
-# Add the directories to the specified archives
-for archive_name, target_dirs in zip(archive_names, dirs_names):
-    # Initialize a ZipFile
-    archive_file = zipfile.ZipFile(archive_name, 'w',
-        compression_method, compresslevel=compression_level)
-    # Log the archive file
-    print("Archiving to {}:".format(os.path.basename(archive_name)))
-
-    # Loop through the directories
-    for target_dir in target_dirs:
-        # Check if the directory exists
-        if os.path.isdir(target_dir):
-            # Get the needed dirnames
-            parent_dir = os.path.dirname(target_dir)
-            archived_dir = os.path.basename(target_dir)
-            os.chdir(parent_dir)
-            # Log the action
-            print("  + {}".format(archived_dir))
-            # Zip the directories relatively to their parent directory
-            zip_dir_rec(archived_dir, archive_file, verbose)
-        else:
-            print("'{}' is not a directory.".format(target_dir))
-
-    # Close the file
-    archive_file.close()
+# Do the job
+run_backup(archive_names, dirs_names, compression_method, compression_level)
 
 
 # Entry point
